da8d43c41e02564501a151ffff7d8334e1a9a7c8
devangg <devangsabbarwal46509@gmail.com>
2025-10-06 21:18:59 +0530

    Hangman game

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,48 @@
+import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(word_list)
+lives = 6
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+print(chosen_word)  # For testing
+print("Current Lives:", lives)
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+# Create placeholder
+word_length = len(chosen_word)
+display = "_" * word_length
+print(display)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    # Build new display
+    new_display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            new_display += letter
+            if guess not in correct_letters:
+                correct_letters.append(guess)
+        elif letter in correct_letters:
+            new_display += letter
+        else:
+            new_display += "_"
+
+    display = new_display
+    print(display)
+
+    # If guess not in chosen word → lose a life
+    if guess not in chosen_word:
+        lives -= 1
+        print("Wrong guess. Lives left:", lives)
+        if lives == 0:
+            game_over = True
+            print("You lose!")
+
+    # If no underscores left → player wins
+    if "_" not in display:
+        game_over = True
+        print("You win!")
